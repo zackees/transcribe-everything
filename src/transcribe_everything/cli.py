52bc5f741c6af06de1988aabef95edc466ce72ac
zackees <z@zackees.com>
2025-03-28 18:10:03 -0700

    Refactor transcribe_everything/cli.py, add transcribe_everything/run.py

--- src/transcribe_everything/cli.py
+++ src/transcribe_everything/cli.py
@@ -4,35 +4,8 @@ Main entry point.
 
 import sys
 
-from virtual_fs import FSPath, Vfs
-
 from transcribe_everything.cli_args import Args
-from transcribe_everything.util import is_media_file
-
-
-def run(args: Args) -> int:
-    vfs_src = Vfs.begin(src=args.src)
-    vfs_dst = Vfs.begin(src=args.dst)
-
-    with vfs_src as src, vfs_dst as dst:
-
-        print(f"src: {src}")
-        print(f"dst: {dst}")
-
-        media_files: list[FSPath] = []
-
-        with vfs_src.walk_begin() as walker:
-            for root, _, files in walker:
-                for file in files:
-                    if is_media_file(file):
-                        media_files.append(root / file)
-                if len(media_files) > 0:
-                    break
-
-        print(f"found {len(media_files)} media files")
-        for media_file in media_files:
-            print(media_file)
-    return 0
+from transcribe_everything.run import run
 
 
 def main() -> int:
---
+++ src/transcribe_everything/run.py
@@ -0,0 +1,45 @@
+"""
+Main entry point.
+"""
+
+import sys
+
+from virtual_fs import FSPath, Vfs
+
+from transcribe_everything.cli_args import Args
+from transcribe_everything.util import is_media_file
+
+
+def run(args: Args) -> int:
+    vfs_src = Vfs.begin(src=args.src)
+    vfs_dst = Vfs.begin(src=args.dst)
+
+    with vfs_src, vfs_dst:
+        media_files: list[FSPath] = []
+
+        with vfs_src.walk_begin() as walker:
+            for root, _, files in walker:
+                for file in files:
+                    if is_media_file(file):
+                        media_files.append(root / file)
+                if len(media_files) > 0:
+                    break
+
+        print(f"found {len(media_files)} media files")
+        for media_file in media_files:
+            print(media_file)
+    return 0
+
+
+def main() -> int:
+    """Main entry point for the template_python_cmd package."""
+    args = Args.parse_args()
+    return run(args)
+
+
+if __name__ == "__main__":
+    src = "dst:TorrentBooks/podcast"
+    dst = "dst:TorrentBooks/podcast"
+    sys.argv.append(src)
+    sys.argv.append(dst)
+    sys.exit(main())
