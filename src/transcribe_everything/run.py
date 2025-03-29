dafa6e9a1694ce551688e13adfd311cd883da36f
zackees <z@zackees.com>
2025-03-28 18:29:45 -0700

    Refactor code for finding and processing media files

--- src/transcribe_everything/run.py
+++ src/transcribe_everything/run.py
@@ -2,28 +2,61 @@
 Main entry point.
 """
 
+from dataclasses import dataclass
+
 from virtual_fs import FSPath, Vfs
 
 from transcribe_everything.args import Args
 from transcribe_everything.util import is_media_file
 
 
-def run(args: Args) -> int:
+def _reduce(files: list[FSPath]) -> list[FSPath]:
+    # take all the media files, remove the suffix, add .txt. If that file
+    # exists then remove it from the list
+    files_set: set[FSPath] = set(files)
+    files_out: list[FSPath] = []
+    for file in files:
+        if file.with_suffix(".txt") in files_set:
+            continue
+        files_out.append(file)
+    return files_out
+
+
+@dataclass
+class Batch:
+    files: list[FSPath]
+    root: FSPath
+
+    def __post_init__(self) -> None:
+        assert isinstance(self.files, list), f"Expected list, got {type(self.files)}"
+        assert isinstance(self.root, FSPath), f"Expected FSPath, got {type(self.root)}"
+
+    def __str__(self) -> str:
+        return f"Batch(root={self.root}, files={self.files})"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+def find_files(args: Args) -> list[Batch]:
     vfs_src = Vfs.begin(src=args.src)
     vfs_dst = Vfs.begin(src=args.dst)
 
     with vfs_src, vfs_dst:
-        media_files: list[FSPath] = []
+        batches: list[Batch] = []
 
         with vfs_src.walk_begin() as walker:
             for root, _, files in walker:
+                media_files: list[FSPath] = []
                 for file in files:
                     if is_media_file(file):
                         media_files.append(root / file)
                 if len(media_files) > 0:
-                    break
+                    batches.append(Batch(files=media_files, root=root))
+    return batches
 
-        print(f"found {len(media_files)} media files")
-        for media_file in media_files:
-            print(media_file)
+
+def run(args: Args) -> int:
+    batches = find_files(args)
+    print(batches)
     return 0
