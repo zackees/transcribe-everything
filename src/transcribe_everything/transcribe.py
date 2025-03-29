4e46fb88e160d2d55ab6635eed55a7237140d4d3
zackees <z@zackees.com>
2025-03-28 18:49:06 -0700

    Update virtual-fs dependency to v1.0.21 and add file checks

--- src/transcribe_everything/transcribe.py
+++ src/transcribe_everything/transcribe.py
@@ -4,7 +4,11 @@ Main entry point.
 
 from virtual_fs import FSPath
 
+from transcribe_everything.util import is_media_file
+
 
 def transcribe(src: FSPath, dst: FSPath) -> None:
     print(f"Transcribing {src} to {dst}")
+    assert is_media_file(src.suffix), f"Expected .mp3, got {src.suffix}"
+    assert dst.suffix == ".txt", f"Expected .txt, got {dst.suffix}"
     # todo: implement transcribe function
