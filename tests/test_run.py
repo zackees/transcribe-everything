ece55ada4653516382059916ab7066b069a98c9b
zackees <z@zackees.com>
2025-03-28 19:10:01 -0700

    Refactor test_run.py with temporary directory usage

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,6 +3,8 @@ Unit test file.
 """
 
 import unittest
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from transcribe_everything.args import Args
 from transcribe_everything.run import run
@@ -15,13 +17,20 @@ class MainTester(unittest.TestCase):
 
     def test_imports(self) -> None:
         """Test command line interface (CLI)."""
-        args: Args = Args(
-            src="dst:TorrentBooks/podcast",
-            max_batches=1,
-            batch_size=1,
-        )
-        rtn = run(args)
-        self.assertEqual(0, rtn)
+
+        with TemporaryDirectory() as tmpdir:
+            cwd: Path = Path(tmpdir)
+            (cwd / "test.mp4").touch()
+            (cwd / "test.txt").touch()
+            (cwd / "test.mp3").touch()
+
+            args: Args = Args(
+                src=cwd.as_posix(),
+                max_batches=1,
+                batch_size=1,
+            )
+            rtn = run(args)
+            self.assertEqual(0, rtn)
 
 
 if __name__ == "__main__":
